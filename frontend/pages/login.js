59c6db71be9cc17d28510367fb174cbd35f55cff
khaledmo99 <khaled.mofleh707@gmail.com>
2024-02-12 11:39:28 +0100

    add btn to go to the home page

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import LoginForm from './../components/AuthForms/LoginForm';
+import React from "react";
+import {View, Text, Image, TouchableOpacity} from "react-native";
+import {useNavigation} from "@react-navigation/native";
+import LoginForm from "./../components/AuthForms/LoginForm";
 
 export default function Login() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <View style={{ marginTop:150 }}>
-          <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-            Connectez<Text style={{ color: '#EC5A13' }}>.</Text>
-          </Text>
-          <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-            Explorez<Text style={{ color: '#EC5A13' }}>.</Text>
-          </Text>
-          <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-            Economisez<Text style={{ color: '#EC5A13' }}>.</Text>
-          </Text>
-        </View>
-        <LoginForm style={{ width: '80%', alignItems: 'center' }} />
+  const navigation = useNavigation();
+
+  const goToHomePage = () => {
+    navigation.navigate("home");
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingBottom: 20,
+      }}
+    >
+      <View style={{marginTop: 150}}>
+        <Text style={{fontSize: 24, fontWeight: "bold"}}>
+          Connectez<Text style={{color: "#EC5A13"}}>.</Text>
+        </Text>
+        <Text style={{fontSize: 24, fontWeight: "bold"}}>
+          Explorez<Text style={{color: "#EC5A13"}}>.</Text>
+        </Text>
+        <Text style={{fontSize: 24, fontWeight: "bold"}}>
+          Economisez<Text style={{color: "#EC5A13"}}>.</Text>
+        </Text>
       </View>
-    );
-  }
+      <LoginForm style={{width: "80%", alignItems: "center"}} />
+      <TouchableOpacity
+        onPress={goToHomePage}
+        style={{
+          marginTop: 20,
+          backgroundColor: "#EC5A13",
+          padding: 10,
+          borderRadius: 5,
+        }}
+      >
+        <Text style={{color: "#FFFFFF"}}>Aller à la page d'accueil</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
